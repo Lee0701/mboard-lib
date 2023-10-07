526168dc037604036478d214361eb8a8e9493526
Lee0701 <hgl1002@naver.com>
2023-10-08 02:22:43 +0900

    Add a permission for receiving text context

--- lib/src/main/java/io/github/lee0701/mboard_lib/conversion/Constants.kt
+++ lib/src/main/java/io/github/lee0701/mboard_lib/conversion/Constants.kt
@@ -6,8 +6,8 @@ object Constants {
     const val ACTION_UPDATE_CONTEXT_TEXT = "io.github.lee0701.mboard.intent.action.UPDATE_CONTEXT_TEXT"
 
     const val PERMISSION_CONVERT_TEXT = "io.github.lee0701.mboard.permission.CONVERT_TEXT"
-    const val PERMISSION_RECEIVE_CONTEXT_TEXT = "io.github.lee0701.mboard.permission.RECEIVE_CONTEXT_TEXT"
     const val PERMISSION_RECEIVE_CONVERTED_TEXT = "io.github.lee0701.mboard.permission.RECEIVE_CONVERTED_TEXT"
+    const val PERMISSION_RECEIVE_CONTEXT_TEXT = "io.github.lee0701.mboard.permission.RECEIVE_CONTEXT_TEXT"
 
     const val EXTRA_TEXT = "io.github.lee0701.mboard.intent.extra.TEXT"
     const val EXTRA_CANDIDATES = "io.github.lee0701.mboard.intent.extra.CANDIDATES"
